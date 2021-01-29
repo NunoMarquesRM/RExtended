9dc64888fe842e978e75108e17b98c84c7262229
Nuno Marques <nuno_marques__1@hotmail.com>
2021-01-29 18:03:07 +0000

    Patch 0.3.1.2
    Added config copper sheet

--- prototypes/satisfactory.lua
+++ prototypes/satisfactory.lua
@@ -1,3 +1,4 @@
+--------------------------------------------------------------------------
 --- Iron Ingot
 local item_iron_ingot = table.deepcopy(data.raw.item['iron-plate'])
 item_iron_ingot.name = "iron-ingot-s1"
@@ -16,7 +17,7 @@ recipe_iron_ingot.enabled = true
 
 
 data:extend({item_iron_ingot,recipe_iron_ingot})
-
+--------------------------------------------------------------------------
 --- Copper Ingot
 local item_copper_ingot = table.deepcopy(data.raw.item['iron-plate'])
 item_copper_ingot.name = "copper-ingot-s1"
@@ -35,23 +36,42 @@ recipe_copper_ingot.enabled = true
 
 
 data:extend({item_copper_ingot,recipe_copper_ingot})
-
-
+--------------------------------------------------------------------------
 --- Iron Plate
-local item_plate_ingot = table.deepcopy(data.raw.item['electronic-circuit'])
-item_plate_ingot.name = "iron-plate-s1"
-item_plate_ingot.icon = "__RExtended__/graphics/icons/Satisfactory/Iron_Plate.png"
-item_plate_ingot.icon_size = 64
-item_plate_ingot.subgroup = "iron-extends"
-item_plate_ingot.order = "b-a"
+local item_iron_plate = table.deepcopy(data.raw.item['electronic-circuit'])
+item_iron_plate.name = "iron-plate-s1"
+item_iron_plate.icon = "__RExtended__/graphics/icons/Satisfactory/Iron_Plate.png"
+item_iron_plate.icon_size = 64
+item_iron_plate.subgroup = "iron-extends"
+item_iron_plate.order = "b-a"
 
-local recipe_plate_ingot = table.deepcopy(data.raw.recipe['electronic-circuit'])
-recipe_plate_ingot.name = "iron-plate-s1"
-recipe_plate_ingot.energy_required = 2
-recipe_plate_ingot.result = "iron-plate-s1"
-recipe_plate_ingot.ingredients = {{"iron-ingot-s1", 3}}
-recipe_plate_ingot.enabled = true
+local recipe_iron_plate = table.deepcopy(data.raw.recipe['electronic-circuit'])
+recipe_iron_plate.name = "iron-plate-s1"
+recipe_iron_plate.energy_required = 2
+recipe_iron_plate.result = "iron-plate-s1"
+recipe_iron_plate.ingredients = {{"iron-ingot-s1", 3}}
+recipe_iron_plate.enabled = true
 
 
 
-data:extend({item_plate_ingot,recipe_plate_ingot})
+data:extend({item_iron_plate,recipe_iron_plate})
+--------------------------------------------------------------------------
+--- Copper Sheet
+local item_copper_sheet = table.deepcopy(data.raw.item['electronic-circuit'])
+item_copper_sheet.name = "copper-sheet-s1"
+item_copper_sheet.icon = "__RExtended__/graphics/icons/Satisfactory/Copper_Sheet.png"
+item_copper_sheet.icon_size = 64
+item_copper_sheet.subgroup = "copper-extends"
+item_copper_sheet.order = "c-a"
+
+local recipe_copper_sheet = table.deepcopy(data.raw.recipe['electronic-circuit'])
+recipe_copper_sheet.name = "copper-sheet-s1"
+recipe_copper_sheet.energy_required = 2
+recipe_copper_sheet.result = "copper-sheet-s1"
+recipe_copper_sheet.ingredients = {{"copper-ingot-s1", 3}}
+recipe_copper_sheet.enabled = true
+
+
+
+data:extend({item_plate_ingot,recipe_copper_sheet})
+--------------------------------------------------------------------------
