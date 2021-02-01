bc39f17f96f04ce2ab10c0501d38eb0933419b23
Nuno Marques <nuno_marques__1@hotmail.com>
2021-02-01 18:56:03 +0000

    Patch 0.3.3
    Added:
    Iron rod; Screw, Wire, Cable;

--- prototypes/satisfactory.lua
+++ prototypes/satisfactory.lua
@@ -53,8 +53,9 @@ local recipe_iron_plate = table.deepcopy(data.raw.recipe['copper-cable'])
 recipe_iron_plate.name = "iron-plate-sat1"
 recipe_iron_plate.icon = "__RExtended__/graphics/icons/Satisfactory/Iron_Plate.png"
 recipe_iron_plate.icon_size = 64
-recipe_iron_plate.energy_required = 2
+recipe_iron_plate.energy_required = 6
 recipe_iron_plate.result = "iron-plate-sat1"
+recipe_iron_plate.result_count = 2
 recipe_iron_plate.ingredients = {{"iron-ingot-sat1", 3}}
 recipe_iron_plate.enabled = true
 recipe_iron_plate.category = "crafting"
@@ -74,13 +75,102 @@ local recipe_copper_sheet = table.deepcopy(data.raw.recipe['copper-cable'])
 recipe_copper_sheet.name = "copper-sheet-sat1"
 recipe_copper_sheet.icon = "__RExtended__/graphics/icons/Satisfactory/Copper_Sheet.png"
 recipe_copper_sheet.icon_size = 64
-recipe_copper_sheet.energy_required = 2
+recipe_copper_sheet.energy_required = 6
 recipe_copper_sheet.result = "copper-sheet-sat1"
-recipe_copper_sheet.ingredients = {{"copper-ingot-sat1", 3}}
+recipe_copper_sheet.ingredients = {{"copper-ingot-sat1", 2}}
 recipe_copper_sheet.enabled = true
 recipe_copper_sheet.category = "crafting"
 recipe_copper_sheet.subgroup = "copper-extends"
 
 
 data:extend({item_copper_sheet,recipe_copper_sheet})
+--------------------------------------------------------------------------
+--- Iron Rod
+local item_iron_rod = table.deepcopy(data.raw.item['copper-cable'])
+item_iron_rod.name = "iron-rod-sat1"
+item_iron_rod.icon = "__RExtended__/graphics/icons/Satisfactory/Iron_Rod.png"
+item_iron_rod.icon_size = 64
+item_iron_rod.subgroup = "iron-extends"
+item_iron_rod.order = "b-b"
+
+local recipe_iron_rod = table.deepcopy(data.raw.recipe['copper-cable'])
+recipe_iron_rod.name = "iron-rod-sat1"
+recipe_iron_rod.icon = "__RExtended__/graphics/icons/Satisfactory/Iron_Rod.png"
+recipe_iron_rod.icon_size = 64
+recipe_iron_rod.energy_required = 4
+recipe_iron_rod.result = "iron-rod-sat1"
+recipe_iron_rod.ingredients = {{"iron-ingot-sat1", 1}}
+recipe_iron_rod.enabled = true
+recipe_iron_rod.category = "crafting"
+recipe_iron_rod.subgroup = "iron-extends"
+
+data:extend({item_iron_rod,recipe_iron_rod})
+--------------------------------------------------------------------------
+--- Screw
+local item_iron_screw = table.deepcopy(data.raw.item['copper-cable'])
+item_iron_screw.name = "iron-screw-sat1"
+item_iron_screw.icon = "__RExtended__/graphics/icons/Satisfactory/Screw.png"
+item_iron_screw.icon_size = 64
+item_iron_screw.subgroup = "iron-extends"
+item_iron_screw.order = "b-c"
+
+local recipe_iron_screw = table.deepcopy(data.raw.recipe['copper-cable'])
+recipe_iron_screw.name = "iron-screw-sat1"
+recipe_iron_screw.icon = "__RExtended__/graphics/icons/Satisfactory/Screw.png"
+recipe_iron_screw.icon_size = 64
+recipe_iron_screw.energy_required = 6
+recipe_iron_screw.result = "iron-screw-sat1"
+recipe_iron_screw.result_count = 4
+recipe_iron_screw.ingredients = {{"iron-rod-sat1", 1}}
+recipe_iron_screw.enabled = true
+recipe_iron_screw.category = "crafting"
+recipe_iron_screw.subgroup = "iron-extends"
+
+data:extend({item_iron_screw,recipe_iron_screw})
+--------------------------------------------------------------------------
+--- Wire
+local item_copper_wire = table.deepcopy(data.raw.item['copper-cable'])
+item_copper_wire.name = "copper-wire-sat1"
+item_copper_wire.icon = "__RExtended__/graphics/icons/Satisfactory/Wire.png"
+item_copper_wire.icon_size = 64
+item_copper_wire.subgroup = "copper-extends"
+item_copper_wire.order = "c-b"
+
+local recipe_copper_wire = table.deepcopy(data.raw.recipe['copper-cable'])
+recipe_copper_wire.name = "copper-wire-sat1"
+recipe_copper_wire.icon = "__RExtended__/graphics/icons/Satisfactory/Wire.png"
+recipe_copper_wire.icon_size = 64
+recipe_copper_wire.energy_required = 6
+recipe_copper_wire.result = "copper-wire-sat1"
+recipe_copper_wire.result_count = 2
+recipe_copper_wire.ingredients = {{"copper-ingot-sat1", 1}}
+recipe_copper_wire.enabled = true
+recipe_copper_wire.category = "crafting"
+recipe_copper_wire.subgroup = "copper-extends"
+
+
+data:extend({item_copper_wire,recipe_copper_wire})
+--------------------------------------------------------------------------
+--- Cable
+local item_copper_cable = table.deepcopy(data.raw.item['copper-cable'])
+item_copper_cable.name = "copper-cable-sat1"
+item_copper_cable.icon = "__RExtended__/graphics/icons/Satisfactory/Cable.png"
+item_copper_cable.icon_size = 64
+item_copper_cable.subgroup = "copper-extends"
+item_copper_cable.order = "c-c"
+
+local recipe_copper_cable = table.deepcopy(data.raw.recipe['copper-cable'])
+recipe_copper_cable.name = "copper-cable-sat1"
+recipe_copper_cable.icon = "__RExtended__/graphics/icons/Satisfactory/Cable.png"
+recipe_copper_cable.icon_size = 64
+recipe_copper_cable.energy_required = 6
+recipe_copper_cable.result = "copper-cable-sat1"
+recipe_copper_cable.result_count = 1
+recipe_copper_cable.ingredients = {{"copper-wire-sat1", 2}}
+recipe_copper_cable.enabled = true
+recipe_copper_cable.category = "crafting"
+recipe_copper_cable.subgroup = "copper-extends"
+
+
+data:extend({item_copper_cable,recipe_copper_cable})
 --------------------------------------------------------------------------
